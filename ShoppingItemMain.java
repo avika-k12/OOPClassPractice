e6c921824b6880e8accee764c73df0b69764c32b
Avika K <a.kapoor20@theabbey.co.uk>
2025-10-01 21:12:16 +0100

    Update ShoppingItemMain.java

--- ShoppingItemMain.java
+++ ShoppingItemMain.java
@@ -1,6 +1,6 @@
 public class ShoppingListMain {
     public static void main(String[] args){
-        ShoppingListClass fantasticItem = new ShoppingListClass("Kure Kure", 0.98f, 10);
+        ShoppingListClass fantasticItem = new ShoppingListClass("Kure Kure", 0.99f, 9);
 
         System.out.println(fantasticItem.calculateTotalCost());
         System.out.println(fantasticItem.updateQuantity(12));
