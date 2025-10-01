cccfbb71eed5db966f8ebfb8db0995f7498e1860
Avika K <a.kapoor20@theabbey.co.uk>
2025-10-01 21:37:10 +0100

    Create TeacherClass.java

---
+++ TeacherClass.java
@@ -0,0 +1,23 @@
+public class TeacherClass {
+    public String name;
+    public String subject;
+    public int yearsOfExperience;
+
+    public TeacherClass(String name, String subject, int yOE){
+        this.name = name;
+        this.subject = subject;
+        this.yearsOfExperience = yOE;
+    }
+
+    public String introduce(){
+        return "This is " + this.name + "!\nThey teach " + this.subject + " and have been teaching for " + this.yearsOfExperience;
+    }
+
+    public String teachLesson(){
+        return this.name + " has begun teaching " + this.subject;
+    }
+
+    public String retire(){
+        return this.name + " has now retired after teaching for " + this.yearsOfExperience + " many years";
+    }
+}
